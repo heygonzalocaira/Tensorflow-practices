c9a37db9a348d65bb1414189a63075bbdd3eecfe
GonzaloCaira <gcairaaguilar@gmail.com>
2021-06-25 22:11:35 -0500

    learned more

--- src/TensorflowDocumentation/avida.py
+++ src/TensorflowDocumentation/avida.py
@@ -36,3 +36,31 @@ with tf.GradientTape() as tape:
 
 grad = tape.gradient(loss, w)
 print(grad)
+
+
+
+#################################################
+(mnist_images, mnist_labels), _ = tf.keras.datasets.mnist.load_data()
+
+dataset = tf.data.Dataset.from_tensor_slices(
+    (tf.cast(mnist_images[...,tf.newaxis]/255,tf.float32),
+    tf.cast(mnist_labels,tf.int64))
+)
+dataset = dataset.shuffle(1000).batch(32)
+
+mnist_model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16,[3,3], activation="relu",input_shape=(None,None,1)),
+    tf.keras.layers.Conv2D(16,[3,3], activation="relu"),
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(10)
+])
+
+for images,labels in dataset.take(1):
+  print("Logits: ", mnist_model(images[0:1]).numpy())
+
+
+optimizer = tf.keras.optimizers.Adam()
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits= True)
+
+loss_history = []
+
