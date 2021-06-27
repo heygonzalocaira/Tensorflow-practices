67d3a277a89f54690c379bf7738d3ef3bcf5e537
GonzaloCaira <gcairaaguilar@gmail.com>
2021-06-26 21:07:05 -0500

    i know how to make checkpoints

--- src/TensorflowDocumentation/avida.py
+++ src/TensorflowDocumentation/avida.py
@@ -144,6 +144,33 @@ status = model.load_weights("weights")
 x = tf.Variable(10.)
 checkpoint = tf.train.Checkpoint(x=x)
 
-x.assign(2.)
+x.assign(2.)# Assign a new value to the variables and save.
+
 checkpoint_path = "./ckpt/"
 checkpoint.save(checkpoint_path)
+
+
+x.assign(11.) # Change the variable after saving.
+
+
+# Restore values from the checkpoint
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_path))
+
+print(x)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16,[3,3], activation = "relu"),
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(10)
+])
+
+optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+checkpoint_dir = "path/to/modeldir"
+
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+checkpoint_prefix = os.path.join(checkpoint_dir,"ckpt")
+root = tf.train.Checkpoint(optimizer = optimizer,
+                            model = model)
+root.save(checkpoint_prefix)
+root.restore(tf.train.latest_checkpoint(checkpoint_dir))
