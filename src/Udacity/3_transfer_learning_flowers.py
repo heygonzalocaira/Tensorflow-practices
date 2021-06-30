a30f67bcf35f542cd479c59dc61f7b7012d887cf
GonzaloCaira <gcairaaguilar@gmail.com>
2021-06-29 22:38:49 -0500

    Learning how to make transfer learning

--- src/Udacity/3_transfer_learning_flowers.py
+++ src/Udacity/3_transfer_learning_flowers.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 import tensorflow_hub as hub
 import tensorflow_datasets as tfds
+from tensorflow.python.client import device_lib 
+print(device_lib.list_local_devices())
 
 from tensorflow.keras import layers
 
@@ -89,3 +91,69 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
+
+
+class_names = np.array(dataset_info.features['label'].names)
+
+print(class_names)
+
+image_batch, label_batch = next(iter(train_batches))
+
+
+image_batch = image_batch.numpy()
+label_batch = label_batch.numpy()
+
+predicted_batch = model.predict(image_batch)
+predicted_batch = tf.squeeze(predicted_batch).numpy()
+
+predicted_ids = np.argmax(predicted_batch, axis=-1)
+predicted_class_names = class_names[predicted_ids]
+
+print(predicted_class_names)
+
+print("Labels:           ", label_batch)
+print("Predicted labels: ", predicted_ids)
+
+plt.figure(figsize=(10,9))
+for n in range(30):
+  plt.subplot(6,5,n+1)
+  plt.subplots_adjust(hspace = 0.3)
+  plt.imshow(image_batch[n])
+  color = "blue" if predicted_ids[n] == label_batch[n] else "red"
+  plt.title(predicted_class_names[n].title(), color=color)
+  plt.axis('off')
+_ = plt.suptitle("Model predictions (blue: correct, red: incorrect)")
+
+IMAGE_RES = 299
+
+(training_set, validation_set), dataset_info = tfds.load(
+    'tf_flowers', 
+    with_info=True, 
+    as_supervised=True, 
+    split=['train[:70%]', 'train[70%:]'],
+)
+train_batches = training_set.shuffle(num_training_examples//4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+validation_batches = validation_set.map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+URL = "https://tfhub.dev/google/tf2-preview/inception_v3/feature_vector/4"
+feature_extractor = hub.KerasLayer(URL,
+  input_shape=(IMAGE_RES, IMAGE_RES, 3),
+  trainable=False)
+
+model_inception = tf.keras.Sequential([
+  feature_extractor,
+  tf.keras.layers.Dense(num_classes)
+])
+
+model_inception.summary()
+
+model_inception.compile(
+  optimizer='adam', 
+  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+EPOCHS = 6
+
+history = model_inception.fit(train_batches,
+                    epochs=EPOCHS,
+                    validation_data=validation_batches)
